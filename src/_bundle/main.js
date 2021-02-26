0f082aa828fdf906bedbf1f16c45e66dd9955850
Dominic Ferris <contactdomferris@gmail.com>
2021-02-25 20:59:08 -0600

    create bg color transitions

--- src/_bundle/main.js
+++ src/_bundle/main.js
@@ -5,6 +5,13 @@ import barbaCss from "barbaCss";
 
 barba.use(barbaCss);
 
+const main = document.querySelector("main");
+
+barba.hooks.before((data) => {
+  const background = data.next.container.dataset.background;
+  main.style.setProperty("--page-background", background);
+});
+
 barba.init({
   transitions: [
     {
