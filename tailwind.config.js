ae3afdbeb78b7be87c3c6bfbe189ab23a0816852
Dominic Ferris <contactdomferris@gmail.com>
2021-03-27 17:42:21 -0500

    update tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,5 +10,11 @@ module.exports = {
     },
   },
   plugins: [require("@tailwindcss/forms"), require("tailwindcss-debug-screens")],
-  purge: ["./src/**/*.js", "./src/**/*.html"],
+  purge: [
+    "./src/**/*.js",
+    "./src/*.html",
+    "./src/**/*.html",
+    "./src/**/**/*.html",
+    "./src/**/*.liquid",
+  ],
 };
